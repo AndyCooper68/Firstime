9a58e7d1d513a0b66166cd65c90249f573ad6d10
AndyCooper68 <ziper1262@gmail.com>
2020-09-07 20:07:04 +0300

    Wasup

---
+++ src/Example.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner (System.in);
+	System.out.println("Provid a number");
+	 int number = scanner.nextInt();
+	 
+	 boolean isPrimary = isPrimary(number);
+	 
+	 System.out.println(isPrimary);
+	 
+	 
+	}
+
+	private static boolean isPrimary(int number) {
+		boolean isThereRemainder = true ;
+		
+		int divider = 2;
+		while(isThereRemainder && divider < number){
+			
+			
+			isThereRemainder= ( number % divider !=0);
+			
+			divider++;
+			
+		}
+		
+		return isThereRemainder;
+		
+	}
+
+}
